f3d772ee06569339b544f5c71d1ec14097074226
siyuniums <t-siyuniu@microsoft.com>
2022-07-14 17:21:33 -0700

    delete print

--- instrumentation/servlet/servlet-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/v3_0/Servlet3OutputStreamWriteBytesAdvice.java
+++ instrumentation/servlet/servlet-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/v3_0/Servlet3OutputStreamWriteBytesAdvice.java
@@ -7,7 +7,6 @@ import static io.opentelemetry.javaagent.instrumentation.servlet.v3_0.snippet.In
 import io.opentelemetry.javaagent.instrumentation.servlet.v3_0.snippet.InjectedInfo;
 import io.opentelemetry.javaagent.instrumentation.servlet.v3_0.snippet.InjectionObject;
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
 import javax.servlet.ServletOutputStream;
 import net.bytebuddy.asm.Advice;
 
@@ -20,9 +19,6 @@ public class Servlet3OutputStreamWriteBytesAdvice {
       throws UnsupportedEncodingException {
     InjectionObject obj = getInjectionObject(servletOutputStream);
     InjectedInfo info = stringInjection(write, 0, write.length, obj);
-
     write = info.bits;
-
-    System.out.println("inside bytesadvice" + new String(write, Charset.defaultCharset()));
   }
 }
--- instrumentation/servlet/servlet-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/v3_0/Servlet3OutputStreamWriteBytesAndOffsetAdvice.java
+++ instrumentation/servlet/servlet-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/v3_0/Servlet3OutputStreamWriteBytesAndOffsetAdvice.java
@@ -7,7 +7,6 @@ import static io.opentelemetry.javaagent.instrumentation.servlet.v3_0.snippet.In
 import io.opentelemetry.javaagent.instrumentation.servlet.v3_0.snippet.InjectedInfo;
 import io.opentelemetry.javaagent.instrumentation.servlet.v3_0.snippet.InjectionObject;
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
 import javax.servlet.ServletOutputStream;
 import net.bytebuddy.asm.Advice;
 
@@ -25,8 +24,5 @@ public class Servlet3OutputStreamWriteBytesAndOffsetAdvice {
     InjectedInfo info = stringInjection(write, off, len, obj);
     write = info.bits;
     len = info.length;
-
-    System.out.println(
-        "inside bytes offset advice" + new String(write, Charset.defaultCharset()) + len);
   }
 }
