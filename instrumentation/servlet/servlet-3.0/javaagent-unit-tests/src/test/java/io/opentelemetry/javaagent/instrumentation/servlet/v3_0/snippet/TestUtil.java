d8129811447727c16872f2ba1f0494aa2eebc99f
siyuniums <t-siyuniu@microsoft.com>
2022-07-19 10:08:02 -0700

    enter change

--- instrumentation/servlet/servlet-3.0/javaagent-unit-tests/src/test/java/io/opentelemetry/javaagent/instrumentation/servlet/v3_0/snippet/TestUtil.java
+++ instrumentation/servlet/servlet-3.0/javaagent-unit-tests/src/test/java/io/opentelemetry/javaagent/instrumentation/servlet/v3_0/snippet/TestUtil.java
@@ -3,7 +3,6 @@ package io.opentelemetry.javaagent.instrumentation.servlet.v3_0.snippet;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 
 public class TestUtil {
   public static String readFile(String resourceName) throws IOException {
@@ -17,6 +16,6 @@ public class TestUtil {
     while ((length = in.read(buffer)) != -1) {
       result.write(buffer, 0, length);
     }
-    return result.toString(StandardCharsets.UTF_8);
+    return result.toString("UTF-8");
   }
 }
--- instrumentation/servlet/servlet-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/v3_0/Servlet3OutputStreamWriteBytesAndOffsetAdvice.java
+++ instrumentation/servlet/servlet-3.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/servlet/v3_0/Servlet3OutputStreamWriteBytesAndOffsetAdvice.java
@@ -6,23 +6,25 @@ import static io.opentelemetry.javaagent.instrumentation.servlet.v3_0.snippet.In
 import io.opentelemetry.javaagent.instrumentation.servlet.v3_0.snippet.InjectedInfo;
 import io.opentelemetry.javaagent.instrumentation.servlet.v3_0.snippet.InjectionObject;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import javax.servlet.ServletOutputStream;
 import net.bytebuddy.asm.Advice;
 
 public class Servlet3OutputStreamWriteBytesAndOffsetAdvice {
-  @Advice.OnMethodEnter(skipOn = Advice.OnDefaultValue.class, suppress = Throwable.class)
+  @Advice.OnMethodEnter(suppress = Throwable.class)
   public static boolean methodEnter(
       @Advice.This ServletOutputStream servletOutputStream,
       @Advice.Argument(value = 0, readOnly = false) byte[] write,
       @Advice.Argument(1) int off,
       @Advice.Argument(value = 2, readOnly = false) int len)
       throws UnsupportedEncodingException {
+    System.out.println(
+        "inside print BytesAndOffsetAdvice" + new String(write, Charset.defaultCharset()));
     InjectionObject obj = getInjectionObject(servletOutputStream);
     InjectedInfo info = obj.stringInjection(write, off, len);
     if (info != null) {
       write = info.bytes;
       len = info.length;
     }
-    return false;
   }
 }
